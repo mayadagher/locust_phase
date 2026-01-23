d2e22188561b759c75a11ecaa117b60218d407bd
Maya Dagher <maya.dagher325@gmail.com>
2026-01-23 14:49:41 +0000

    Organizational changes, functions that compute over all batches, better data acquisition from volumes.

--- src/visualize_time.py
+++ src/visualize_time.py
@@ -10,20 +10,21 @@ from helper_fns import *
 
 '''_____________________________________________________PLOTTING FUNCTIONS____________________________________________________________'''
 
-def detections_over_time(path_h5:str):
+def detections_over_time(path_h5:str, plot:bool=False):
     num_detections = []
     with h5py.File(path_h5, 'r') as f:
         for i in range(1, len(f.keys()) + 1):
             frame = f[f'coords_{i}']
             num_detections.append(frame.shape[1])
     
-    _, ax = plt.subplots(2, 1, figsize=(20, 10))
-    ax[0].plot(num_detections)
-    ax[1].plot(100*np.array(num_detections)/np.max(num_detections))
-    ax[1].set_xlabel('Frame', fontsize=17)
-    ax[0].set_ylabel('Number of detections', fontsize=17)
-    ax[1].set_ylabel('Percent detected (%)', fontsize=17)
-    plt.savefig('./plots/20230329/all/detections_over_time.png')
+    if plot:
+        _, ax = plt.subplots(2, 1, figsize=(20, 10))
+        ax[0].plot(num_detections)
+        ax[1].plot(100*np.array(num_detections)/np.max(num_detections))
+        ax[1].set_xlabel('Frame', fontsize=17)
+        ax[0].set_ylabel('Number of detections', fontsize=17)
+        ax[1].set_ylabel('Percent detected (%)', fontsize=17)
+        plt.savefig('./plots/20230329/all/detections_over_time.png')
 
     return num_detections
 
